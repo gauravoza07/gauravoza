4bded816d98a2c87bc426e9c9fbe22cf544e157d
Gaurav <ozagaurav03@gmail.com>
2025-03-01 22:16:48 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   
