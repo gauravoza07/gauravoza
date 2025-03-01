1a9a941cab59b2174c7db4ece0558e408229b392
Gaurav <ozagaurav03@gmail.com>
2025-03-01 23:11:42 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,42 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <>
-      {/* <!-- Preloader Area Start --> */}
-      <div className="preloader">
-        <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
-          <path
-            id="preloaderSvg"
-            d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"
-          ></path>
-        </svg>
-
-        <div className="preloader-heading">
-          <div className="load-text">
-            <span>G</span>
-            <span>A</span>
-            <span>U</span>
-            <span>R</span>
-            <span>A</span>
-            <span>V</span>
-          </div>
-        </div>
-      </div>
-      {/* <!-- Preloader Area End --> */}
-
-      {/* <!-- start: Back To Top --> */}
-      <div className="progress-wrap" id="scrollUp">
-        <svg
-          className="progress-circle svg-content"
-          width="100%"
-          height="100%"
-          viewBox="-1 -1 102 102"
-        >
-          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
-        </svg>
-      </div>
-      {/* <!-- end: Back To Top --> */}
-
-      {/* <!-- HEADER START --> */}
+      {/* HEADER START */}
       <header className="tj-header-area header-absolute">
         <div className="container">
           <div className="row">
