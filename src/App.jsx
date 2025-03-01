f17c52ff80d806efff6a88b40054e6cd607fc736
Gaurav <ozagaurav03@gmail.com>
2025-03-01 22:26:15 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   
   return (
     <>
-    <Header />
+    <Header/>
     <main>
       <h1>Hello I am Gaurav Oza.</h1>
       <h3>Site coming soon...</h3>
